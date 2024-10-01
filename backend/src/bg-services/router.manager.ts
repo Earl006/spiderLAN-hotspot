19e5df56aa2ff341a9ab8f6099689dde75a07400
Earl006 <earljoe06@gmail.com>
2024-10-01 16:20:37 +0300

    [Added]: login.html as the redirect page

--- backend/src/bg-services/router.manager.ts
+++ backend/src/bg-services/router.manager.ts
@@ -384,7 +384,7 @@ class RouterManager {
                 '=dns-name=hotspot.spiderlan.net',
                 '=html-directory=hotspot',
                 '=login-by=http-pap',
-                `=login-page=https://stxtuning.co.uk/pop-and-bang-remap/`,
+                `=login-page=hotspot/login.html`,
 
             ]);
             console.log('Hotspot profile created');
